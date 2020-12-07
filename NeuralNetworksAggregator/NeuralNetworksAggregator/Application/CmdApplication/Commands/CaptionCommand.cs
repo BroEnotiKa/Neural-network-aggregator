43e513c2550a4b88e53774bb44cf3695c3c7ee01
BroEnotiKa <57271872+BroEnotiKa@users.noreply.github.com>
2020-12-07 23:12:38 +0500

    Add files via upload
    Make commands for bot and autoparse them

---
+++ NeuralNetworksAggregator/NeuralNetworksAggregator/Application/CmdApplication/Commands/CaptionCommand.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+
+namespace NeuralNetworksAggregator.Application.CmdApplication.Commands
+{
+    public class CaptionCommand : ConsoleCommand
+    {
+        private readonly NeuralNetworksAggregator aggregator;
+        private readonly TextWriter writer;
+
+        public CaptionCommand(NeuralNetworksAggregator aggregator, TextWriter writer)
+            : base("caption", "caption <filePath>      # get caption by photo")
+        {
+            this.aggregator = aggregator;
+            this.writer = writer;
+        }
+
+        public override void Execute(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                writer.WriteLine($"Wrong arguments. Try help {Name}");
+            }
+            var caption = aggregator.GetCaptionPhotoFrom(args[0]);
+            writer.WriteLine($"Caption:\n{caption}");
+        }
+    }
+}
